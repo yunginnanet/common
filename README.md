7222e460b190c8bebb7e41e6ed0b1bb70ede10b2
kayos@tcp.direct <kayos@tcp.direct>
2022-01-01 14:01:21 -0800

    Add: random string, docs

--- README.md
+++ README.md
@@ -4,18 +4,28 @@
 
 stuff I use a lot.
 
-
 #### func  Abs
 
 ```go
 func Abs(n int) int
 ```
+Abs will give you the positive version of a negative integer, quickly.
+
+#### func  BytesToBlake2b
+
+```go
+func BytesToBlake2b(b []byte) []byte
+```
+BytesToBlake2b ignores all errors and gives you a blakae2b 64 hash value as a
+byte slice. (or panics somehow)
 
 #### func  CompareChecksums
 
 ```go
 func CompareChecksums(a []byte, b []byte) bool
 ```
+CompareChecksums will take in two byte slices, hash them with blake2b, and tell
+you if the resulting checksums match.
 
 #### func  Fprint
 
@@ -29,21 +39,24 @@ Fprint is fmt.Fprint with error handling.
 ```go
 func RNG(n int) int
 ```
+RNG is a Random Number Generator
 
-Random Number Generator (uses a combo of crypto/rand and math/rand for better performance)
+#### func  RandSleepMS
+
+```go
+func RandSleepMS(n int)
+```
+RandSleepMS sleeps for a random period of time with a maximum of n milliseconds.
+
+#### func  RandStr
+
+```go
+func RandStr(size int) string
+```
+RandStr generates a random alphanumeric string with a max length of size.
 
 #### func  RangeIterate
 
 ```go
 func RangeIterate(ips interface{}) chan *ipa.IP
 ```
-
-IP Address iteration
-
-#### func  SnoozeMS
-
-```go
-func SnoozeMS(n int)
-```
-
-Random sleep, max of n seconds.
--- util.go
+++ util.go
@@ -1,17 +1,18 @@
 package common
 
 import (
-	"fmt"
-	"io"
-
 	crip "crypto/rand"
 	"encoding/binary"
+	"fmt"
+	"io"
 	"math/rand"
 	"time"
 
 	"github.com/rs/zerolog/log"
 	"golang.org/x/crypto/blake2b"
 )
+
+const charset = "abcdefghijklmnopqrstuvwxyz1234567890"
 
 // Fprint is fmt.Fprint with error handling.
 func Fprint(w io.Writer, s string) {
@@ -21,18 +22,21 @@ func Fprint(w io.Writer, s string) {
 	}
 }
 
-func bytesToHash(b []byte) []byte {
+// BytesToBlake2b ignores all errors and gives you a blakae2b 64 hash value as a byte slice. (or panics somehow)
+func BytesToBlake2b(b []byte) []byte {
 	Hasha, _ := blake2b.New(64, nil)
 	Hasha.Write(b)
 	return Hasha.Sum(nil)
 }
 
+// CompareChecksums will take in two byte slices, hash them with blake2b, and tell you if the resulting checksums match.
 func CompareChecksums(a []byte, b []byte) bool {
-	ahash := bytesToHash(a)
-	bhash := bytesToHash(b)
+	ahash := BytesToBlake2b(a)
+	bhash := BytesToBlake2b(b)
 	return string(ahash) == string(bhash)
 }
 
+// RNG is a Random Number Generator
 func RNG(n int) int {
 	var seed int64
 	err := binary.Read(crip.Reader, binary.BigEndian, &seed)
@@ -43,10 +47,21 @@ func RNG(n int) int {
 	return rng.Intn(n)
 }
 
-func SnoozeMS(n int) {
+// RandStr generates a random alphanumeric string with a max length of size.
+func RandStr(size int) string {
+	buf := make([]byte, size)
+	for i := 0; i != size; i++ {
+		buf[i] = charset[uint32(RNG(32))%uint32(len(charset))]
+	}
+	return string(buf)
+}
+
+// RandSleepMS sleeps for a random period of time with a maximum of n milliseconds.
+func RandSleepMS(n int) {
 	time.Sleep(time.Duration(RNG(n)) * time.Millisecond)
 }
 
+// Abs will give you the positive version of a negative integer, quickly.
 func Abs(n int) int {
 	// ayyee smash 6ros
 	n64 := int64(n)
