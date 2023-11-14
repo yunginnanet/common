fc8a72e617539cd8de95afe728e88b3bbba7a6f5
kayos@tcp.direct <kayos@tcp.direct>
2023-11-14 14:29:25 -0800

    Feat/Refactor[hash]: !!BREAKING!! add CRC32 and general refactor !!BREAKING!!

--- hash/hash_test.go
+++ hash/hash_test.go
@@ -4,10 +4,11 @@ import (
 	"bytes"
 	"encoding/base64"
 	"os"
+	"path/filepath"
+	"strings"
 	"testing"
 
 	"git.tcp.direct/kayos/common/entropy"
-	"git.tcp.direct/kayos/common/squish"
 )
 
 const (
@@ -16,89 +17,96 @@ const (
 	kayosSHA1    = "M23ElC0sQYAK+MaMZVmza2L8mss="
 	kayosSHA256  = "BagY0TmoGR3O7t80BGm4K6UHPlqEg6HJirwQmhrPK4U="
 	kayosSHA512  = "xiuo2na76acrWXCTTR++O1pPZabOhyj8nbfb5Go3e1pEq9VJYIsOioTXalf2GCuERmFecWkmaL5QI8mIXXWpNA=="
+	kayosCRC32   = "xtig5w=="
 )
 
-func TestBlake2bsum(t *testing.T) {
-	og := squish.B64d(kayosBlake2b)
-	newc := Blake2bSum([]byte("kayos\n"))
-	if !bytes.Equal(newc, og) {
-		t.Fatalf("wanted: %v, got %v", kayosBlake2b, squish.B64e(newc))
-	}
-	if !BlakeEqual([]byte("kayos\n"), []byte{107, 97, 121, 111, 115, 10}) {
-		t.Fatalf("BlakeEqual should have been true. %s should == %s", []byte("kayos\n"), []byte{107, 97, 121, 111, 115, 92, 110})
-	}
-	t.Logf("[blake2bSum] success: %s", kayosBlake2b)
-}
-
-func TestBlakeFileChecksum(t *testing.T) {
-	path := t.TempDir() + "/blake2b.dat"
-	err := os.WriteFile(path, []byte{107, 97, 121, 111, 115, 10}, os.ModePerm)
-	if err != nil {
-		t.Errorf("[FAIL] failed to write test fle for TestBlakeFileChecksum: %s", err.Error())
-	}
-	filecheck, err2 := BlakeFileChecksum(path)
-	if err2 != nil {
-		t.Errorf("[FAIL] failed to read test fle for TestBlakeFileChecksum: %s", err2.Error())
-	}
-	if len(filecheck) == 0 {
-		t.Errorf("[FAIL] got nil output from BlakeFileChecksum")
-	}
-	if !bytes.Equal(filecheck, squish.B64d(kayosBlake2b)) {
-		t.Fatalf("[FAIL] wanted: %v, got %v", kayosBlake2b, squish.B64e(filecheck))
-	}
-	badfile, err3 := BlakeFileChecksum(t.TempDir() + "/" + entropy.RandStr(50))
-	if err3 == nil {
-		t.Errorf("[FAIL] shouldn't have been able to read phony file")
-	}
-	if len(badfile) != 0 {
-		t.Errorf("[FAIL] got non-nil output from bogus file: %v", badfile)
-	}
-	if !bytes.Equal(filecheck, squish.B64d(kayosBlake2b)) {
-		t.Fatalf("[FAIL] wanted: %v, got %v", kayosBlake2b, squish.B64e(filecheck))
-	}
-	err = os.WriteFile(path+".empty", []byte{}, os.ModePerm)
-	if err != nil {
-		t.Errorf("[FAIL] failed to write test fle for TestBlakeFileChecksum: %s", err.Error())
-	}
-	_, err4 := BlakeFileChecksum(path + ".empty")
-	if err4 == nil {
-		t.Fatalf("[FAIL] should have failed to read empty file")
-	}
-}
+var kayosByteSlice = []byte{107, 97, 121, 111, 115, 10}
 
 func TestSum(t *testing.T) {
+	t.Parallel()
 	if Sum(TypeNull, []byte("yeet")) != nil {
 		t.Fatal("Sum(TypeNull, []byte(\"yeet\")) should have returned nil")
 	}
 
 	var (
-		ogsha1, _   = base64.StdEncoding.DecodeString(kayosSHA1)
-		ogsha256, _ = base64.StdEncoding.DecodeString(kayosSHA256)
-		ogsha512, _ = base64.StdEncoding.DecodeString(kayosSHA512)
-		ogmd5, _    = base64.StdEncoding.DecodeString(kayosMD5)
-		newsha1     = Sum(TypeSHA1, []byte("kayos\n"))
-		newsha256   = Sum(TypeSHA256, []byte("kayos\n"))
-		newsha512   = Sum(TypeSHA512, []byte("kayos\n"))
-		newmd5      = Sum(TypeMD5, []byte("kayos\n"))
+		ogsha1, _    = base64.StdEncoding.DecodeString(kayosSHA1)
+		ogsha256, _  = base64.StdEncoding.DecodeString(kayosSHA256)
+		ogsha512, _  = base64.StdEncoding.DecodeString(kayosSHA512)
+		ogmd5, _     = base64.StdEncoding.DecodeString(kayosMD5)
+		ogBlake2b, _ = base64.StdEncoding.DecodeString(kayosBlake2b)
+		ogCRC32, _   = base64.StdEncoding.DecodeString(kayosCRC32)
+		valids       = map[Type][]byte{
+			TypeSHA1:    ogsha1,
+			TypeSHA256:  ogsha256,
+			TypeSHA512:  ogsha512,
+			TypeMD5:     ogmd5,
+			TypeCRC32:   ogCRC32,
+			TypeBlake2b: ogBlake2b,
+		}
 	)
-
-	if !bytes.Equal(newsha1, ogsha1) {
-		t.Fatalf("[sha1] wanted: %v, got %v", ogsha1, newsha1)
+	for k, v := range valids {
+		typeToTest := k
+		valueToTest := v
+		t.Run(typeToTest.String()+"/string_check", func(t *testing.T) {
+			t.Parallel()
+			if !strings.EqualFold(typeToTest.String(), StringToType(typeToTest.String()).String()) {
+				t.Errorf("[FAIL] %s: wanted %s, got %s", typeToTest.String(), typeToTest.String(), StringToType(typeToTest.String()).String())
+			}
+		})
+		t.Run(typeToTest.String()+"/static_check", func(t *testing.T) {
+			t.Parallel()
+			mySum := Sum(typeToTest, kayosByteSlice)
+			if !bytes.Equal(mySum, valueToTest) {
+				t.Errorf("[FAIL] %s: wanted %v, got %v", typeToTest.String(), valueToTest, mySum)
+			}
+		})
+		t.Run(typeToTest.String()+"/file_check", func(t *testing.T) {
+			t.Parallel()
+			path := filepath.Join(t.TempDir(), typeToTest.String()) // for coverage
+			if err := os.WriteFile(path, kayosByteSlice, os.ModePerm); err != nil {
+				t.Fatalf("[FAIL] failed to write test fle for TestSum: %s", err.Error())
+			}
+			res, err := SumFile(typeToTest, path)
+			if err != nil {
+				t.Fatalf("[FAIL] failed to read test fle for TestSum: %s", err.Error())
+			}
+			if !bytes.Equal(res, valueToTest) {
+				t.Errorf("[FAIL] %s: wanted %v, got %v", typeToTest.String(), valueToTest, res)
+			}
+		})
 	}
-	t.Logf("[sha1]   success: %s", kayosSHA1)
+	t.Run("bad file", func(t *testing.T) {
+		t.Parallel()
+		_, err := SumFile(TypeSHA1, "/dev/null")
+		if err == nil {
+			t.Fatal("SumFile should have returned an error")
+		}
+		if _, err = SumFile(TypeSHA1, entropy.RandStrWithUpper(500)); err == nil {
+			t.Fatal("SumFile should have returned an error")
+		}
+	})
+	t.Run("unknown type", func(t *testing.T) {
+		t.Parallel()
+		if Type(uint8(94)).String() != "unknown" {
+			t.Fatal("Type(uint(9453543)).String() should have returned \"unknown\"")
+		}
+		if StringToType(entropy.RandStr(10)) != TypeNull {
+			t.Fatal("bogus string should have returned TypeNull")
+		}
+	})
+}
 
-	if !bytes.Equal(newsha256, ogsha256) {
-		t.Fatalf("[sha256] wanted: %v, got %v", ogsha256, newsha256)
-	}
-	t.Logf("[sha256] success: %s", kayosSHA256)
+var benchData = []byte(entropy.RandStrWithUpper(5000))
 
-	if !bytes.Equal(newsha512, ogsha512) {
-		t.Fatalf("[sha512] wanted: %v, got %v", ogsha512, newsha512)
+func BenchmarkSum(b *testing.B) {
+	for _, sumType := range []Type{TypeSHA1, TypeSHA256, TypeSHA512, TypeBlake2b, TypeMD5} {
+		b.Run(sumType.String(), func(b *testing.B) {
+			b.ReportAllocs()
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				b.SetBytes(int64(len(benchData)))
+				Sum(sumType, benchData)
+			}
+		})
 	}
-	t.Logf("[sha512] success: %s", kayosSHA512)
-
-	if !bytes.Equal(newmd5, ogmd5) {
-		t.Fatalf("[md5] wanted: %v, got %v", ogmd5, newmd5)
-	}
-	t.Logf("[md5]    success: %s", kayosMD5)
 }
